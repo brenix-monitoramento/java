f034128fa7c78c5a1abcf7688451bfc7d973b446
Breno Gonçalves <breno.goncalves@datatraffic.com.br>
2026-04-08 09:44:26 -0300

    ajuste de static

--- banco/src/entities/Account.java
+++ banco/src/entities/Account.java
@@ -3,7 +3,7 @@ package entities;
 
 public class Account {
     private String name;
-    private double balance = 0.0;
+    private static final double BALANCE = 0.0;
 
     public void setName(String name){
         this.name = name;
@@ -14,6 +14,6 @@ public class Account {
     }
 
     public double getBalance(){
-        return this.balance;
+        return BALANCE;
     }
 }
